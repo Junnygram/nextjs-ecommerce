ab06c4d530231c4d218161ba7950c219c82c23cb
Junnygram <you@example.com>
2023-04-19 11:53:13 +0100

    implemented login API

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,8 +4,12 @@ import React, { useContext, useState, useEffect } from 'react';
 import { Store } from '../utils/Store';
 import { FaRegUser } from 'react-icons/fa';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useSession } from 'next-auth/react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Layout({ title, children }) {
+  const { status, data: session } = useSession();
   const year = new Date().getFullYear();
   const { state } = useContext(Store);
   const { cart } = state;
@@ -20,6 +24,7 @@ function Layout({ title, children }) {
         <meta name="description" content="Ecommerce Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ToastContainer position="bottom-center" limit={1} />
       <div className="flex min-h-screen flex-col justify-between ">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
@@ -37,15 +42,20 @@ function Layout({ title, children }) {
                   )}
                 </span>
               </Link>
-              {/* <Link href="/login">
-                <div className="p-2">Login </div>
-              </Link> */}
-              <Link href="/login" className="flex pt-2 pl-2">
-                {/* <div className=" text-sm ">Login</div> */}
-                <div className="pt-[2px]">
-                  <FaRegUser />
-                </div>
-              </Link>
+
+              <div className="pt-2">
+                {status === 'loading' ? (
+                  'Loading'
+                ) : session?.user ? (
+                  session.user.name
+                ) : (
+                  <Link href="/login" className="flex pt-2 pl-2">
+                    <div className="">
+                      <FaRegUser />
+                    </div>
+                  </Link>
+                )}
+              </div>
             </div>
           </nav>
         </header>
